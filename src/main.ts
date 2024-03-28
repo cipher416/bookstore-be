f6ea092119f657cb2f4ba282bfac412f698662da
Cristoper Anderson <cristoper.anderson@binus.ac.id>
2024-03-28 22:59:59 +0700

    chore: readd swagger at /api

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // import { json } from 'express';
 // import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('Bookstore API')
-  //   .setDescription('')
-  //   .setVersion('1.0')
-  //   .addTag('bookstore')
-  //   .build();
+  const config = new DocumentBuilder()
+    .setTitle('Bookstore API')
+    .setDescription('')
+    .setVersion('1.0')
+    .addTag('bookstore')
+    .build();
   // // app.use(cookieParser());
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(8000);
 }
 bootstrap();
