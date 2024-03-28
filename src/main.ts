87f9043f47df9efff54cd9ee6b88f10365b797b7
Cristoper Anderson <cristoper.anderson@binus.ac.id>
2024-03-28 22:13:36 +0700

    -

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-// import { json } from 'express';
-// import * as cookieParser from 'cookie-parser';
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
 
+import cors from 'cors';
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(cors());
   // const config = new DocumentBuilder()
   //   .setTitle('Bookstore API')
   //   .setDescription('')
