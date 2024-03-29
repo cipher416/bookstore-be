7cf9bfb054fa0a40256a199b78c45705017e6bab
Cristoper Anderson <cristoper.anderson@binus.ac.id>
2024-03-29 08:31:35 +0700

    -

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // import { json } from 'express';
 // import * as cookieParser from 'cookie-parser';
 
-async function bootstrap() {
+export default async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   const config = new DocumentBuilder()
